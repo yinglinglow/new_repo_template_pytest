43b2e0e10fe35a659ca7d8b7331910f805bfc4a3
Ying Ling <low.yingling@gmail.com>
2018-07-27 18:09:18 +0800

    Create test_main.py

---
+++ test_main.py
@@ -0,0 +1,31 @@
+"""
+to run tests: 
+C:\ProgramData\Anaconda3\python.exe -m unittest discover -s tests -p 'test*.py'
+"""
+
+import pathlib
+import unittest
+import sys
+import pandas as pd
+from pandas.util.testing import assert_frame_equal
+
+from main import test_function
+
+path = str(pathlib.Path(__file__).parents[1]) # generate path to parent dir
+if path not in sys.path:
+    sys.path.append(path)
+
+class Test_main(unittest.TestCase):
+
+    def test_test_function(self):
+        # import end result to test against
+        path = str(pathlib.Path(__file__).parents[1]) # generates path to proj_etd
+        test_filepath = ''
+        
+        # generate file using imported function
+        
+        # test
+        assert_frame_equal(test, check)
+        
+if __name__ == '__main__':
+    unittest.main()
