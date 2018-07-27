bd7cc0f2a6995173565159f9fcc6c58631e26813
Ying Ling <low.yingling@gmail.com>
2018-07-27 18:06:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 """
 to run:
-python main.py
+C:\ProgramData\Anaconda3\python.exe main.py
 
 """
-
+import sys
 import pandas as pd
 
 def sample_get_column_names(df):
@@ -11,8 +11,17 @@ def sample_get_column_names(df):
     return col_names
 
 if __name__ == '__main__':
-
-    df = pd.read_csv('abc.csv')
+    # for debug
+    # filepath = 'C:\\blahblah\\filename.csv'
+    # sys.stdout = open("C:/check/stdout.txt", "w")
+    # sys.stderr = open("C:/check/stderr.txt", "w")
+    # end debug
+    
+    # read in variables
+    filepath = sys.argv[1]
+    
+    # run function
+    df = pd.read_csv(filepath)
     col_names = sample_get_column_names(df)
 
     print(col_names)
